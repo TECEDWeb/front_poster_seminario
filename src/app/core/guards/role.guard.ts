884752adfb18b04711572be7eb279a93157c82fe
Jefferson Pozo <misaelpozocatuto@gmail.com>
2026-07-01 17:18:37 -0500

    role guard actualizado

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,13 +1,13 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
-import { StorageService } from '../services/storage.service';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-export const roleGuard: CanActivateFn = async (route) => {
+export const roleGuard: CanActivateFn = (route) => {
 
   const router = inject(Router);
-  const storage = inject(StorageService);
+  const authService = inject(AuthService);
 
-  const usuario = await storage.getUsuario();
+  const usuario = authService.obtenerUsuario();
 
   if (!usuario) {
     router.navigate(['/login']);
@@ -23,19 +23,6 @@ export const roleGuard: CanActivateFn = async (route) => {
     return true;
   }
 
-  switch (usuario.rol) {
-
-    case 'admin':
-      router.navigate(['/admin/dashboard']);
-      break;
-
-    case 'evaluador':
-      router.navigate(['/evaluador/dashboard']);
-      break;
-
-    default:
-      router.navigate(['/login']);
-  }
-
+  router.navigate(['/login']);
   return false;
 };
